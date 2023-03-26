c9dce17b8eb90295871ea39b26070cfa663ecf1f
Kalina Frontend <dzhiltcov@yandex.ru>
2023-03-26 16:10:29 +0200

    feat: подключиться к Mongo

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mynewdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+app.use(express.static(path.join((__dirname, 'public'))));
+
+app.get('/', (req, res) => {
+  res.send('hello12');
+});
+
+app.listen(PORT, () => {
+  console.log('hello))))');
+});
