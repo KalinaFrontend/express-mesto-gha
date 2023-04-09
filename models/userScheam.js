d413060becaed58cd34332a8ff87c0a33081c64e
Kalina Frontend <dzhiltcov@yandex.ru>
2023-04-09 22:22:57 +0200

    fix: исправить авторизацию

--- models/userScheam.js
+++ models/userScheam.js
@@ -3,64 +3,72 @@ const { compare } = require('bcryptjs');
 const isEmail = require('validator/lib/isEmail');
 const { regexImageLink } = require('../utils/constants');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    default: 'Жак-Ив Кусто',
-    validate: {
-      validator: ({ length }) => length >= 2 && length <= 30,
-      message: 'Имя пользователя должно быть длиной от 2 до 30 символов',
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      default: 'Жак-Ив Кусто',
+      validate: {
+        validator: ({ length }) => length >= 2 && length <= 30,
+        message: 'Имя пользователя должно быть длиной от 2 до 30 символов',
+      },
+    },
+    about: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 30,
+      default: 'Исследователь',
+    },
+    avatar: {
+      type: String,
+      required: true,
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+      validate: {
+        validator: (avatar) => regexImageLink.test(avatar),
+        message: 'Неверный формат ссылки аватара',
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (email) => isEmail(email),
+        message: 'Неверный формат аватара email',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
     },
   },
-  about: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-    default: 'Исследователь',
-  },
-  avatar: {
-    type: String,
-    required: true,
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    validate: {
-      validator: (avatar) => regexImageLink.test(avatar),
-      message: 'Неверный формат ссылки аватара',
+
+  {
+    versionKey: false,
+    statics: {
+      findUserByCredentials(email, password) {
+        return this
+          .findOne({ email })
+          .select('+password')
+          .then((user) => {
+            if (user) {
+              return compare(password, user.password)
+                .then((matched) => {
+                  if (matched) return user;
+
+                  return Promise.reject();
+                });
+            }
+
+            return Promise.reject();
+          });
+      },
     },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: (email) => isEmail(email),
-      message: 'Неверный формат аватара email',
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-});
 
-userSchema.statics.findUserByCredentials = function findUser(email, password) {
-  return this.findOne({ email }).select('+password')
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль.'));
-      }
-
-      return compare(password, user.password).then((matched) => {
-        // проверяем хеши паролей]
-        if (!matched) {
-          return Promise.reject(new Error('Неправильные почта или пароль.'));
-        }
-
-        return user;
-      });
-    });
-};
+);
 
 module.exports = mongoose.model('User', userSchema);
