17baf13f1a6964fbbad162d87d5408b83503081e
Kalina Frontend <dzhiltcov@yandex.ru>
2023-04-10 22:24:50 +0200

    feat: реализовать централизованную обработку ошибок

--- models/userScheam.js
+++ models/userScheam.js
@@ -7,7 +7,6 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
       default: 'Жак-Ив Кусто',
       validate: {
         validator: ({ length }) => length >= 2 && length <= 30,
@@ -16,14 +15,12 @@ const userSchema = new mongoose.Schema(
     },
     about: {
       type: String,
-      required: true,
       minlength: 2,
       maxlength: 30,
       default: 'Исследователь',
     },
     avatar: {
       type: String,
-      required: true,
       default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
       validate: {
         validator: (avatar) => regexImageLink.test(avatar),
